15844def38763248e8eee1742722b00c6613a16c
Karina Smoje Gueico <karinasmoje@gmail.com>
2018-06-27 17:05:13 -0300

    Prueba de interfaz

---
+++ app.js
@@ -0,0 +1,16 @@
+
+/*const inputText = document.querySelector("input") 
+inputText.addEventListener("keypress", (event)=> {
+    let key = event.which || event.keyCode;
+    if (key ===13) { // 13 es válido para enter
+    let bip = inputText.value; //cuando apreto enter, rescato el valor del input.
+    console.log(bip)  
+    inputText.value = "" ;
+fetch(`http:bip.franciscocapone.com/api/getSaldo/${id_tarjeta}`) 
+.then(response => response.json())
+.then(data => {
+console.log(data); //Información para mi.
+renderInfo(data);
+})
+}
+})*/
